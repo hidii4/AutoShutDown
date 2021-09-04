7ca05780326e6752a232423e22d99c1413d28f25
hidii4 <62072274+hidii4@users.noreply.github.com>
2021-09-04 20:15:09 +0200

    Add files via upload

---
+++ AutoShutDown/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace AutoShutDown
+{
+    partial class Autoshutdown
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(25, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(25, 113);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(191, 26);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pozostały czas: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "(m)\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Podaj czas do wyłaczenia komputera";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(128, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Autoshutdown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 278);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Autoshutdown";
+            this.Text = "Auto ShutDown";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AutoShutDown/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace AutoShutDown
+{
+    public partial class Autoshutdown : Form
+    {
+        public Autoshutdown()
+        {
+            InitializeComponent();
+        }
+
+        private int sec = 0;
+        private int min = 0;
+        private int a = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int value;
+
+            if (int.TryParse(s, out value))
+            {
+                sec = (value * 60);
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button1.Enabled = false;
+                
+                textBox1.Enabled = false;
+                progressBar1.Enabled = true;
+                progressBar1.Maximum = sec;
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            else
+            {
+                label1.Text = "Nieprawidłowe dane wejściowe";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            min = sec / 60;
+            int temp = (sec-- % 60);
+            if(temp < 10)
+            {
+                label1.Text = "Pozostały czas: " + min.ToString() + ":0" + temp.ToString();
+            }
+            else
+            {
+                label1.Text = "Pozostały czas: " + min.ToString() + ":" + temp.ToString();
+            }
+            
+            if(sec == 0)
+            {
+                timer1.Stop();
+                var psi = new ProcessStartInfo("shutdown", "/s /t 0");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+            }
+
+            progressBar1.Value = a++;
+            
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button3.Enabled = false;
+            button3.Text = "Pause";
+            textBox1.Enabled = true;
+            label1.Text = "Pozostały czas: ";
+            button2.Enabled = false;
+            progressBar1.Enabled = false;
+            progressBar1.Value = 0;
+            a = 0;
+            min = 0;
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(button3.Text == "Pause")
+            {
+                timer1.Stop();
+                progressBar1.Enabled = false;
+                button3.Text = "Resume";
+            }
+            else
+            {
+                timer1.Start();
+                progressBar1.Enabled = true;
+                button3.Text = "Pause";
+            }
+            
+        }
+    }
+}
